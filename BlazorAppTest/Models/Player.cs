2060904f6b9c51529131404f0bdbdf4bbae55be7
Thierry Morre <thierry.morre@cognitic.be>
2025-04-08 11:23:59 +0200

    Ajoutez des fichiers projet.

---
+++ BlazorAppTest/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorAppTest.Models
+{
+    public class Player
+    {
+        public Player(int id, string prenom, int score)
+        {
+            Id = id;
+            Prenom = prenom;
+            Score = score;
+        }
+
+        public int Id { get; set; }
+        public string Prenom { get; set; }
+        public int Score { get; set; }
+    }
+}
